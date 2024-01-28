6d67ec89413b016b3eaadb76d3df2fbff341a6b3
VinyasSahai02 <142114422+VinyasSahai02@users.noreply.github.com>
2024-01-28 23:25:52 +0530

    Add files via upload

---
+++ notes-1a/index.js
@@ -0,0 +1,14 @@
+const logEvents=require('./logEvents')
+const EventEmitter = require('events')
+class MyEmitter extends EventEmitter{}
+
+//initialize object
+const myEmitter = new MyEmitter();
+
+//add a listener for the log event
+myEmitter.on('log',(msg)=>logEvents(msg))
+
+setTimeout(()=>{
+    //emit event
+    myEmitter.emit('log','log event emitted!')
+},2000)
